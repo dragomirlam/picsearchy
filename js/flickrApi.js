65894815720a60e1b0e5cf1a693820bdc7e1d813
Lampros Dragomoiris <dragomirlam@gmail.com>
2015-03-05 00:06:42 +0100

    New approach of handling the responses from Flickr API

--- js/flickrApi.js
+++ js/flickrApi.js
@@ -2,36 +2,45 @@ function jsonFlickrApi(res){
 	var photosLength = res.photos.photo.length;
 	var photos = res.photos.photo;
 	var URLs = [];
-	var href, img, imgSmall, imgBig, url, farm_id, server_id, photo_id, secret;
+	var href, img, image, imgBig, url, farm_id, server_id, photoId, secret;
+	var modalWindow;
 
 	document.getElementById("images-container").innerHTML = null;
 	document.getElementById("modal-windows").innerHTML = null;
 
+	var searchResults = {};
+
+	searchResults.images = [];
+	searchResults.key = "search-results";
+
 	for(var i=0; i<photosLength; i++){
+		var img = {};
 		//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 		farm_id = photos[i].farm;
 		server_id = photos[i].server;
-		photo_id = photos[i].id;
+		photoId = photos[i].id;
 		secret = photos[i].secret;
-		s_url = 'https://farm'+farm_id+'.staticflickr.com/'+server_id+'/'+photo_id+'_'+secret+'_q.jpg';
-		b_url = 'https://farm'+farm_id+'.staticflickr.com/'+server_id+'/'+photo_id+'_'+secret+'.jpg';
+
+		img.id = photos[i].id;
+		img.s_url = 'https://farm'+farm_id+'.staticflickr.com/'+server_id+'/'+photoId+'_'+secret+'_s.jpg';
+		img.q_url = 'https://farm'+farm_id+'.staticflickr.com/'+server_id+'/'+photoId+'_'+secret+'_q.jpg';
+		img.b_url = 'https://farm'+farm_id+'.staticflickr.com/'+server_id+'/'+photoId+'_'+secret+'.jpg';
+		searchResults.images.push(img);
 		// img = '<a href="'+ b_url +'">'+'<img alt="'+ photos[i].title +'"src="'+ s_url +'"/>'+'</a>';
-		imgSmall = '<img id="'+photo_id+'" style="border: solid 2px #000000;" onclick="selectImg('+photo_id+')" alt="'+ photos[i].title +'"src="'+ s_url +'" class="small-img"/>';
-		// href = '<a href="#'+photo_id+'" id="pop">'+imgSmall+'</a>';
-		href = imgSmall;
+		image = '<img id="'+photoId+'" style="border: solid 2px #000000;" onclick="selectImg('+photoId+')" src="'+ img.q_url +'" class="img-space"/>';
+		// href = '<a href="#'+photoId+'" id="pop">'+image+'</a>';
+		// href = image;
 
-		imgBig = '<img alt="'+ photos[i].title +'"src="'+ b_url +'"/>';
-		modalWindow = '<a href="#" class="overlay" id="'+photo_id+'"></a>'+
-					  '<div class="popup">'+
-					  imgBig+
-					  '<a class="close" href="#"></a>'+
-					  '</div>';
+		// imgBig = '<img alt="'+ photos[i].title +'"src="'+ b_url +'"/>';
+		// modalWindow = '<a href="#" class="overlay" id="'+photoId+'"></a>'+
+		// 			  '<div class="popup">'+
+		// 			  imgBig+
+		// 			  '<a class="close" href="#"></a>'+
+		// 			  '</div>';
 					  
-		document.getElementById("images-container").innerHTML+= href;
-		document.getElementById("modal-windows").innerHTML+= modalWindow;
-	}
-
-	for(var i=0; i<photosLength; i++){
-		document.getElementById(photo_id).style.visibility = "visible";
+		document.getElementById("images-container").innerHTML+= image;
+		// document.getElementById("modal-windows").innerHTML+= modalWindow;
+		sessionStorage.setItem(searchResults.key, JSON.stringify(searchResults));
+		// console.log(imgURLs);
 	}
 }
