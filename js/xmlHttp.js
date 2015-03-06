ac8a7b5ac4ebc7a26aedcd3522b7f17e9db91230
Lampros Dragomoiris <dragomirlam@gmail.com>
2015-03-06 01:54:19 +0100

    Deletion of old files

--- js/xmlHttp.js
+++
@@ -1,8 +0,0 @@
-function xmlHttpGet(theUrl){
-    var xmlHttp = new XMLHttpRequest();
-
-    //open(method,url,async)
-    xmlHttp.open("GET", theUrl, false);
-    xmlHttp.send();
-    return xmlHttp.responseText;
-}
